97d48801f512dc25136b6060e8e24ca98dd7150d
YuraThunder <iu.stepanov@click-group.ru>
2023-05-08 03:46:22 +0300

    swagger

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
+import swager from '../swagger/output.json';
 
 function App() {
   return (
     <div className="App">
-      <SwaggerUI url="../swagger/output.json" />
+      <SwaggerUI url={`${swager}`} />
     </div>
   );
 }
