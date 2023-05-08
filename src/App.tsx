8ba57157bf0f603a66a707efe91141d090bd64ce
YuraThunder <iu.stepanov@click-group.ru>
2023-05-08 04:08:06 +0300

    swagger

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
-import swager from '../swagger/output.json';
 
 function App() {
   return (
     <div className="App">
-      <SwaggerUI url={`/home/xenoxil/swagger/swagger/output.css`} />
+      <SwaggerUI url={`./output.json`} />
     </div>
   );
 }
